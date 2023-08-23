0f17a3a24dbf5007a9643fd8447d6c319d5f5b0b
Rijan Dhakal <rijan_dhakal@outlook.com>
2023-08-23 12:45:35 -0500

    Changes to the extract_mean_mass function to account for non-numerical values in mass feature representation

--- metabolomics_source_scripts.R
+++ metabolomics_source_scripts.R
@@ -2,7 +2,7 @@
 ## function
 
 extract_mean_mass <- function(x,decimals = 6){
-    bounds <- strsplit(x, "_")[[1]]
+    bounds <- strsplit(sub("^.", "", x), "_")[[1]]
     return(round(mean(as.numeric(bounds[1:2])),decimals))
 }
 
@@ -10,5 +10,5 @@ extract_mean_mass <- function(x,decimals = 6){
 # Extract time from the mass header in the pmsf data (for example "99.08079_99.0806600670743_24.75") into numerical masses.
 extract_pmsf_time <- function(x){
     bounds <- strsplit(x, "_")[[1]]
-    return(bounds[3]) # working under the assumption that the third digit is the time value
+    return(as.numeric(bounds[3])) # working under the assumption that the third digit is the time value
 }
