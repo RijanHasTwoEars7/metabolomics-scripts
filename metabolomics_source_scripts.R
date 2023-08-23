8fcc1898263baaf3e81870adec378fe9642e70a3
Rijan Dhakal <rijan_dhakal@outlook.com>
2023-08-23 15:18:02 -0500

    A new funciton `count_zeros` that: 1. Takes in an arbritary number of vectors 2. Converts the vectors in a data table 3. Tallies the number of 0s per row 4. Returns that tally table

--- metabolomics_source_scripts.R
+++ metabolomics_source_scripts.R
@@ -12,3 +12,14 @@ extract_pmsf_time <- function(x){
     bounds <- strsplit(x, "_")[[1]]
     return(as.numeric(bounds[3])) # working under the assumption that the third digit is the time value
 }
+
+# count the number of rows in a data frame like object with no zeros
+# The way for this to work is to take in an arbritary number of vector-like objects of the same lenght and make a data table like object out of them and count the number of rows with non zero objects in them
+
+# TODO add a way to make sure that all the vectors are of the same length
+count_zeroes <- function(...,x = 0){
+    current_table <- as.data.table(cbind(...))
+    count_of_zeros <- rowSums(current_table == x)
+    unique_table <- table(count_of_zeros)
+    return(unique_table)
+}
