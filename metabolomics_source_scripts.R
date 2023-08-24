dfe64e0be2ce4c2076f0f359cfc58c86d5bc245c
Rijan Dhakal <rijan_dhakal@outlook.com>
2023-08-24 13:02:35 -0500

    Adding the function to calculate the most likely formula

--- metabolomics_source_scripts.R
+++ metabolomics_source_scripts.R
@@ -23,3 +23,24 @@ count_zeroes <- function(...,x = 0){
     unique_table <- table(count_of_zeros)
     return(unique_table)
 }
+
+# the setup to calculate the most likely formulas for a given mass
+
+myElements <- c("C","H","N","O","S","P")
+
+best_fit_mf <- function(mean_mass){
+  df <- calcMF(mz = mean_mass, z = 1, ppm = 5, top = NULL,
+  elements = Rdisop::initializeElements(myElements), maxCounts = TRUE,
+  SeniorRule = TRUE, HCratio = TRUE, moreRatios = TRUE,
+  elementHeuristic = TRUE, Filters = list(DBErange = c(-5, 40)
+  , minElements = "C0H0N0S0O0", maxElements = "C40H60N20S20O20", parity = "e", maxCounts =
+  TRUE, SENIOR3 = 0, HCratio = TRUE, moreRatios = TRUE, elementHeuristic =
+  TRUE), summarize = FALSE, BPPARAM = NULL)
+
+  if(is.null(df)){
+    return('NA')
+  }else{
+    return(df[order(abs(df$ppm)),][1,2])
+  }
+
+}
