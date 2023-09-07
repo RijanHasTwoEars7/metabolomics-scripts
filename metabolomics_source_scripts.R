d6cc82c6e8ced20dd49185f233c1a959f688e2da
Rijan Dhakal <rijan.dhakal@skiff.com>
2023-09-07 17:35:46 +0000

    better naming scheme for one of the funcitons

--- metabolomics_source_scripts.R
+++ metabolomics_source_scripts.R
@@ -17,7 +17,7 @@ extract_pmsf_time <- function(x){
 # The way for this to work is to take in an arbritary number of vector-like objects of the same lenght and make a data table like object out of them and count the number of rows with non zero objects in them
 
 # TODO add a way to make sure that all the vectors are of the same length
-count_zeroes <- function(...,x = 0){
+tabulate_zeroes <- function(...,x = 0){
     current_table <- as.data.table(cbind(...))
     count_of_zeros <- rowSums(current_table == x)
     unique_table <- table(count_of_zeros)
